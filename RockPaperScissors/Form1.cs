17f8250fd1a745ba2396189dd43ed74830c5c046
Om Patel <om31digital@gmail.com>
2024-11-12 11:26:52 -0500

    shortest code!

--- RockPaperScissors/Form1.cs
+++ RockPaperScissors/Form1.cs
@@ -10,11 +10,6 @@ using System.Windows.Forms;
 using System.Media;
 using System.Threading;
 
-/// <summary>
-/// A rock, paper, scissors game that utilizes basic methods
-/// for repetitive tasks.
-/// </summary>
-
 namespace RockPaperScissors
 {
     public partial class Form1 : Form
@@ -22,20 +17,11 @@ namespace RockPaperScissors
         string playerChoice, cpuChoice;
 
         int wins, losses, ties = 0;
-        int choicePause = 1000;
-        int outcomePause = 3000;
 
         Random randGen = new Random();
 
         SoundPlayer jabPlayer = new SoundPlayer(Properties.Resources.jabSound);
         SoundPlayer gongPlayer = new SoundPlayer(Properties.Resources.gong);
-
-        Image rockImage = Properties.Resources.rock168x280;
-        Image paperImage = Properties.Resources.paper168x280;
-        Image scissorImage = Properties.Resources.scissors168x280;
-        Image winImage = Properties.Resources.winTrans;
-        Image loseImage = Properties.Resources.loseTrans;
-        Image tieImage = Properties.Resources.tieTrans;
 
         public Form1()
         {
@@ -45,28 +31,25 @@ namespace RockPaperScissors
         private void rockButton_Click(object sender, EventArgs e)
         {
             playerChoice = "rock";
-            playerImage.Image = rockImage;
+            playerImage.Image = Properties.Resources.rock168x280;
             ComputerTurn();
             DetermineWinner();
-            RoundOver();
         }
 
         private void paperButton_Click(object sender, EventArgs e)
         {
             playerChoice = "paper";
-            playerImage.Image = paperImage;
+            playerImage.Image = Properties.Resources.paper168x280;
             ComputerTurn();
             DetermineWinner();
-            RoundOver();
         }
 
         private void scissorsButton_Click(object sender, EventArgs e)
         {
             playerChoice = "scissor";
-            playerImage.Image = scissorImage;
+            playerImage.Image = Properties.Resources.scissors168x280;
             ComputerTurn();
             DetermineWinner();
-            RoundOver();
         }
 
         public void ComputerTurn()
@@ -77,53 +60,49 @@ namespace RockPaperScissors
             if (choice == 1)
             {
                 cpuChoice = "rock";
-                cpuImage.Image = rockImage;
+                cpuImage.Image = Properties.Resources.rock168x280;
             }
             else if (choice == 2)
             {
                 cpuChoice = "paper";
-                cpuImage.Image = paperImage;
+                cpuImage.Image = Properties.Resources.paper168x280; ;
             }
             else
             {
                 cpuChoice = "scissor";
-                cpuImage.Image = scissorImage;
+                cpuImage.Image = Properties.Resources.scissors168x280;
             }
-        }
-
-        public void RoundOver()
-        {
-            playerImage.Refresh();
-            cpuImage.Refresh();
-            resultImage.Refresh();
-            Thread.Sleep(choicePause);
-            gongPlayer.Play();
-            Thread.Sleep(outcomePause);
-            playerImage.Image = null;
-            cpuImage.Image = null;
-            resultImage.Image = null;
         }
 
         public void DetermineWinner()
         {
             if ((playerChoice == "rock" && cpuChoice == "scissor") || (playerChoice == "paper" && cpuChoice == "rock") || (playerChoice == "scissor" && cpuChoice == "paper"))
             {
-                resultImage.Image = winImage;
+                resultImage.Image = Properties.Resources.winTrans;
                 wins++;
                 winsLabel.Text = $"Wins: {wins}";
             }
             else if ((playerChoice == "rock" && cpuChoice == "paper") || (playerChoice == "paper" && cpuChoice == "scissor") || (playerChoice == "scissor" && cpuChoice == "rock"))
             {
-                resultImage.Image = loseImage;
+                resultImage.Image = Properties.Resources.loseTrans;
                 losses++;
                 lossesLabel.Text = $"Losses: {losses}";
             }
             else if ((playerChoice == "rock" && cpuChoice == "rock") || (playerChoice == "paper" && cpuChoice == "paper") || (playerChoice == "scissor" && cpuChoice == "scissor"))
             {
-                resultImage.Image = tieImage;
+                resultImage.Image = Properties.Resources.tieTrans;
                 ties++;
                 tiesLabel.Text = $"Ties: {ties}";
             }
+            playerImage.Refresh();
+            cpuImage.Refresh();
+            resultImage.Refresh();
+            Thread.Sleep(1000);
+            gongPlayer.Play();
+            Thread.Sleep(2000);
+            playerImage.Image = null;
+            cpuImage.Image = null;
+            resultImage.Image = null;
         }
     }
 }
